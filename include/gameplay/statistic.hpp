128b2fa43765e2e416797ec18b789e8cb4591a12
Connor Garey <connorgarey1999@gmail.com>
2025-12-16 20:18:00 +0000

    Added Statistic

---
+++ include/gameplay/statistic.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include <cstdint>
+
+namespace Gameplay {
+class Statistic 
+{
+  public:
+    Statistic(std::int32_t current_stat = 25000, std::int32_t maximum_stat = 50000);
+    auto increaseStat(std::int32_t increase_amount) noexcept -> void;
+    auto reduceStat(std::int32_t reduce_amount) noexcept -> std::int32_t;
+
+  private:
+    std::int32_t max_{50000};
+    std::int32_t current_{250000};
+};
+}
