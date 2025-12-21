63105d46caab155ab7a6fd133b95bef488b57374
Connor Garey <connorgarey1999@gmail.com>
2025-12-21 23:16:30 +0000

    Added VBO

--- include/engine/opengl/vbo.hpp
+++ include/engine/opengl/vbo.hpp
@@ -1,10 +1,38 @@
 #pragma once 
 
-namespace Engine 
+#include "glad/glad.h"
+
+#include <span>
+#include <cstdint>
+#include <cassert>
+
+namespace Engine::OGL 
 {
+  template<typename BufferType>
   class VBO 
   {
     public:
+      VBO(std::span<const BufferType> vertex_buffer)
+        : vbo_{bindAndGenVbo()} 
+      {
+        createBuffer(vertex_buffer);
+      }
+
     private:
+      std::uint32_t vbo_{0};
+      
+      auto bindAndGenVbo() noexcept -> void
+      {
+        glGenBuffers(1, &vbo_);
+      }
+
+      auto createBuffer(std::span<const BufferType> vertex_buffer) noexcept -> void
+      {
+        assert(vertex_buffer.data() != nullptr);
+        glBufferData(GL_ARRAY_BUFFER,
+                     vertex_buffer.size() * sizeof(BufferType),
+                     vertex_buffer.data(),
+                     GL_STATIC_DRAW);
+      }
   };
 }
