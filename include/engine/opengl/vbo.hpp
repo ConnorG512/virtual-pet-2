1446238b657584331047b91f7e1015240a7caf92
Connor Garey <connorgarey1999@gmail.com>
2025-12-22 15:42:25 +0000

    Using the BufferType from the template

--- include/engine/opengl/vbo.hpp
+++ include/engine/opengl/vbo.hpp
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include "glad/glad.h"
+#include "engine/opengl/gl-types.hpp"
 
 #include <span>
 #include <cstdint>
@@ -8,14 +9,17 @@
 
 namespace Engine::OGL 
 {
-  template<typename BufferType>
   class VBO 
   {
     public:
-      VBO(std::span<const BufferType> vertex_buffer)
-        : vbo_{bindAndGenVbo()} 
+      VBO(std::span<const Engine::OGL::BufferType> vertex_buffer)
       {
+        bindAndGenVbo();
         createBuffer(vertex_buffer);
+      }
+      auto get() const -> std::uint32_t
+      {
+        return vbo_;
       }
 
     private:
@@ -26,7 +30,7 @@ namespace Engine::OGL
         glGenBuffers(1, &vbo_);
       }
 
-      auto createBuffer(std::span<const BufferType> vertex_buffer) noexcept -> void
+      auto createBuffer(std::span<const Engine::OGL::BufferTypee> vertex_buffer) noexcept -> void
       {
         assert(vertex_buffer.data() != nullptr);
         glBufferData(GL_ARRAY_BUFFER,
