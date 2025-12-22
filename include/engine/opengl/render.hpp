52b93c6a633ba27d044f4b9f24abbdf1c5b34e14
Connor Garey <connorgarey1999@gmail.com>
2025-12-22 21:10:27 +0000

    Replaced contents of render with a glclear function.
    .

--- include/engine/opengl/render.hpp
+++ include/engine/opengl/render.hpp
@@ -1,21 +1,9 @@
 #pragma once 
 
 #include "glad/glad.h"
-#include "engine/file-object.hpp"
-
-#include <cstdint>
-#include <span>
-#include <cassert>
-#include <vector>
 
 namespace Engine::OGL 
 {
-  struct ProgramProperties 
-  {
-    const char* vertex_shader_path {};
-    const char* fragment_shader_path {};
-  };
-  auto createProgram(const ProgramProperties& properties) noexcept -> std::uint32_t;
-  auto attachAndLinkToProgram(std::uint32_t program, std::vector<std::uint32_t> shader_list) noexcept -> void;
-  auto deleteShaders(std::vector<std::uint32_t> shader_list) noexcept -> void;
+  auto clearScreen(
+      GLfloat red = 0.0f, GLfloat green = 0.0f, GLfloat blue = 0.0f, GLbitfield mask = GL_COLOR_BUFFER_BIT) noexcept -> void;
 }
